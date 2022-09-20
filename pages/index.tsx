a1476ea27f7a1d5c173d59db4e0dc00c586a77c3
MadMan24 <mdverdera@yahoo.com>
2022-09-20 18:09:57 +0800

    Footer section

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import Menu from "@components/Menu";
-import ButtonSeeAll from "@components/ButtonSeeAll";
 import Items, { itemsImageSource } from "@components/Items";
 import { v4 as uuidv4 } from "uuid";
 
@@ -134,6 +133,134 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      <footer className="bg-black">
+        {/* Container */}
+        <div className="container max-w-6xl py-10 mx-auto">
+          {/* Footer flex container */}
+          <div className="flex flex-col items-center mb-8 space-y-6 md:flex-row md:space-y-0 md:justify-between md:items-start">
+            {/* Menu and Logo Container */}
+            <div className="flex flex-col items-center space-y-8 md:items-start md:space-y-4">
+              {/* Logo */}
+              <div className="h-8">
+                <picture>
+                  <img
+                    src="/img/logo.svg"
+                    alt="logo"
+                    className="w-44 md:scroll-ml-32"
+                  />
+                </picture>
+              </div>
+
+              {/* Menu Container */}
+              <div className="flex flex-col items-center space-y-4 font-bold text-white md:flex-row md:space-y-0 md:space-x-6 md:ml-3">
+                {/* Item 1 */}
+                <div className="h-10 group">
+                  <Link href="#">
+                    <a>About</a>
+                  </Link>
+                  <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
+                </div>
+
+                {/* Item 2 */}
+                <div className="h-10 group">
+                  <Link href="#">
+                    <a>Careers</a>
+                  </Link>
+                  <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
+                </div>
+
+                {/* Item 3 */}
+                <div className="h-10 group">
+                  <Link href="#">
+                    <a>Events</a>
+                  </Link>
+                  <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
+                </div>
+
+                {/* Item 4 */}
+                <div className="h-10 group">
+                  <Link href="#">
+                    <a>Products</a>
+                  </Link>
+                  <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
+                </div>
+
+                {/* Item 5 */}
+                <div className="h-10 group">
+                  <Link href="#">
+                    <a>Support</a>
+                  </Link>
+                  <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
+                </div>
+              </div>
+            </div>
+
+            {/* Social and Copy Container */}
+            <div className="flex flex-col items-start justify-between space-y-4 text-gray-500 ">
+              {/* Icons Container */}
+              <div className="flex items-center justify-center mx-auto space-x-4 md:justify-end md:mx-0">
+                {/* Icon 1 */}
+                <div className="h-8 group">
+                  <Link href={"#"} passHref>
+                    <picture>
+                      <img
+                        src="/img/icon-facebook.svg"
+                        alt="facebook"
+                        className="h-6"
+                      />
+                    </picture>
+                  </Link>
+                </div>
+
+                {/* Icon 2 */}
+                <div className="h-8 group">
+                  <Link href={"#"} passHref>
+                    <picture>
+                      <img
+                        src="/img/icon-twitter.svg"
+                        alt="twitter"
+                        className="h-6"
+                      />
+                    </picture>
+                  </Link>
+                </div>
+
+                {/* Icon 1 */}
+                <div className="h-8 group">
+                  <Link href={"#"} passHref>
+                    <picture>
+                      <img
+                        src="/img/icon-pinterest.svg"
+                        alt="pinterest"
+                        className="h-6"
+                      />
+                    </picture>
+                  </Link>
+                </div>
+
+                {/* Icon 1 */}
+                <div className="h-8 group">
+                  <Link href={"#"} passHref>
+                    <picture>
+                      <img
+                        src="/img/icon-instagram.svg"
+                        alt="instagram"
+                        className="h-6"
+                      />
+                    </picture>
+                  </Link>
+                </div>
+              </div>
+
+              {/* Copyright Container */}
+              <div className="font-bold">
+                &copy; 2022 Loopstudios. All Rights Reserved.
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
